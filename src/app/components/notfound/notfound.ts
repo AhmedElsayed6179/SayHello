93810a59b4873b9f4695924e7d243db46a5cf6f6
Ahmed Elsayed <ahmedelsayed6179@gmail.com>
2025-12-26 16:10:16 +0200

    Add error handling for 404 page with translations

--- src/app/components/notfound/notfound.ts
+++ src/app/components/notfound/notfound.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-notfound',
-  imports: [RouterLink],
+  imports: [RouterLink, TranslateModule],
   templateUrl: './notfound.html',
   styleUrl: './notfound.css',
 })
