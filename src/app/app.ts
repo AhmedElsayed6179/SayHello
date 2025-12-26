595c02f17419bc09486786f134eee64cd1d9620b
Ahmed Elsayed <ahmedelsayed6179@gmail.com>
2025-12-26 18:13:05 +0200

    Refactor layout and language handling for RTL support and improve scrollbar styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { Footer } from './components/footer/footer';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +12,32 @@ import { Footer } from './components/footer/footer';
 })
 export class App implements OnInit {
   protected readonly title = signal('SayHello');
-
+  currentLang = localStorage.getItem('lang') || 'en';
+  private translate = inject(TranslateService);
+  
   ngOnInit() {
     const lang = localStorage.getItem('lang') || 'en';
     document.documentElement.lang = lang;
     document.documentElement.dir = lang === 'ar' ? 'rtl' : 'ltr';
+  }
+
+  toggleLang() {
+    this.currentLang = this.currentLang === 'en' ? 'ar' : 'en';
+    localStorage.setItem('lang', this.currentLang);
+    this.translate.use(this.currentLang);
+
+    // غير اتجاه المحتوى فقط، Footer يتحرك مع main تلقائي
+    const pageContent = document.querySelector('#page-content');
+    if (pageContent) {
+      pageContent.setAttribute('dir', this.currentLang === 'ar' ? 'rtl' : 'ltr');
+    }
+
+    // ممكن تضيف Footer مع main لو حابب
+    const pageFooter = document.querySelector('#page-footer');
+    if (pageFooter) {
+      pageFooter.setAttribute('dir', this.currentLang === 'ar' ? 'rtl' : 'ltr');
+    }
+
+    document.documentElement.lang = this.currentLang;
   }
 }
