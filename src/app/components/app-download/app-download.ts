ab4dd9b8aeff40a490d69e1712e0e183324740e0
Ahmed Elsayed <ahmedelsayed6179@gmail.com>
2026-03-30 01:37:09 +0200

    Update App-Download

--- src/app/components/app-download/app-download.ts
+++ src/app/components/app-download/app-download.ts
@@ -1,13 +1,21 @@
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
+import { ChatService } from '../../service/chat-service';
 
 @Component({
   selector: 'app-app-download',
-  imports: [TranslatePipe, CommonModule],
+  imports: [TranslatePipe, CommonModule, AsyncPipe],
   templateUrl: './app-download.html',
   styleUrl: './app-download.scss',
 })
 export class AppDownload {
-  get isDarkMode(): boolean { return document.body.classList.contains('dark-mode'); }
+  connectedUsers$: Observable<number>;
+  constructor(private chatService: ChatService) {
+    this.connectedUsers$ = this.chatService.connectedUsers$;
+  }
+  get isDarkMode(): boolean {
+    return document.body.classList.contains('dark-mode');
+  }
 }
